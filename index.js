03b053c4c066d17adf40782f7e4e4037d3839467
Otieno Harrison Omondi <ogaloharrison@gmail.com>
2022-08-24 01:58:04 +0300

    commit tree 86469df598002a1688a3ae19d821cd82fbc79e6a

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Add your code here
+function submitData (name, email){
+  const formData = {name, email}
+  return fetch("http://localhost:3000/users",{
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(formData),
+  })
+  .then(res => res.json())
+  .then(user => {document.body.innerHTML = user.id})
+  .catch(err => {document.body.innerHTML = err.message})
+}
